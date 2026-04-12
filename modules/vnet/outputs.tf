48fa6ed9a050bbf932adc178d8daf85a0435e169
Tushar Verma <tusharrverma16@gmail.com>
2026-04-12 10:46:37 +0530

    Polished outputs.tf

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,19 +1,19 @@
 output "vnet_id" {
-  description = "The ID of the VNet"
+  description = "The ID of the created Virtual Network"
   value       = azurerm_virtual_network.this.id
 }
 
 output "vnet_name" {
-  description = "The name of the VNet"
+  description = "The name of the created Virtual Network"
   value       = azurerm_virtual_network.this.name
 }
 
 output "subnet_ids" {
-  description = "Map of subnet IDs (key = subnet name)"
+  description = "Map of all subnet IDs (key = subnet name, e.g. app, private-endpoints)"
   value       = { for k, v in azurerm_subnet.this : k => v.id }
 }
 
 output "nsg_id" {
-  description = "NSG ID (if created)"
+  description = "ID of the Network Security Group (null if create_nsg = false)"
   value       = var.create_nsg ? azurerm_network_security_group.this[0].id : null
 }
